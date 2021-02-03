2e9dba831edef984ae72d058345ae2c965b51787
mamo312 <78474741+mamo312@users.noreply.github.com>
2021-02-03 09:51:14 +0000

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -5,16 +5,16 @@ except:
         # Set true if its VPS
         IS_VPS = False
         
-        API_HASH = ""
-        API_ID = 0
-        BOT_TOKEN = ""
-        BASE_URL_OF_BOT = ""
+        API_HASH = "295d449480357269a6f7d74b83fff771"
+        API_ID = 2898344
+        BOT_TOKEN = "1628431160:AAEPKetrpUyXxWAufx_vim4KNCE3B4NX5mM"
+        BASE_URL_OF_BOT = "https://tortool323.herokuapp.com"
 
         # Edit the server port if you want to keep it default though.
         SERVPORT = 80
 
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
-        ALD_USR = []
+        ALD_USR = [-1001267728460,885488992]
         
         # Google Drive Index Link should include the base dir also See readme for more info
         GD_INDEX_URL = False
@@ -35,13 +35,13 @@ except:
         REMAINING_STR = "▱"
 
         # DB URI for access
-        DB_URI = "dbname=tortk user=postgres password=your-pass host=127.0.0.1 port=5432"
-        
+        DB_URI = "postgres://ngipzgoimwphje:d1f9ce525ddee70ed102ca2b75c81bce9cb94817a10a357bb3bfe576b129d02c@ec2-54-170-123-247.eu-west-1.compute.amazonaws.com:5432/d6nl7mpcm9is4l"
+        https://drake.dewitt.workers.dev/ALLINONE
         # UNCOMMENT THE BELOW LINE WHEN USING CONTAINER AND COMMENT THE UPPER LINE
         #DB_URI = "dbname=tortk user=postgres password=your-pass host=db port=5432"
         
         # The base direcory to which the files will be upload if using RCLONE
-        RCLONE_BASE_DIR = "/"
+        RCLONE_BASE_DIR = "ALLINONE/"
 
         # This value will be considered only if Rclone is True - this may be defied now ;)
         # Cuz at least one needs to be Ture at a time either RCLONE or Leech.
